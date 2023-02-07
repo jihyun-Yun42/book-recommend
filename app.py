86e5c18eb747f171bf6835f0ea3175d4b60fda6a
Ji Hyun Yun <jyun9842@gmail.com>
2023-02-07 12:25:44 +0900

    venv 다시

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ db = client.book_recommend
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('',headers=headers)
-
-soup = BeautifulSoup(data.text, 'html.parser')
 
 
 @app.route('/')
@@ -79,4 +75,4 @@ def join():
     return jsonify({'msg': 'POST(기록) 연결 완료!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
+   app.run('0.0.0.0', port=5002, debug=True)
